5fe9eb518fdee6c4602d40d2404c91955f6b877f
Avneet singh <93909103+avneets2103@users.noreply.github.com>
2022-05-08 15:09:15 +0530

    Recursion 1

---
+++ Questions.cpp
@@ -0,0 +1,13 @@
+//1. Given an array of length N, you need to find and return the sum of all elements of the array.
+//2. Given an array of length N and an integer x, you need to find if x is present in the array or not. Return true or false.
+//3. Given an array of length N and an integer x, you need to find and return the first index of integer x present in the array. Return -1 if it is not present in the array.
+//   First index means, the index of first occurrence of x in the input array.
+//4. Given an array of length N and an integer x, you need to find and return the last index of integer x present in the array. Return -1 if it is not present in the array.
+//   Last index means - if x is present multiple times in the array, return the index at which x comes last in the array.
+//5.Given an array of length N and an integer x, you need to find all the indexes where x is present in the input array. Save all the indexes in an array (in increasing order). 
+//6. Write a program to find x to the power n (i.e. x^n). Take x and n from the user. You need to return the answer.
+//7. Given two integers M & N, calculate and return their multiplication using recursion. You can only use subtraction and addition for your calculation. No other operators are allowed.
+//8. Given an integer N, count and return the number of zeros that are present in the given integer using recursion.
+//9. Sum till kth term of gp with a=1 and r = 1/2
+//10.Check if the number is a pallindrome or not
+//11.Find sum of all digits from entered integer recursively.
